5b84691f4da7e89096afea4664e1b9c72e9bce9a
Thomas F. K. Jorna <hello@tefkah.com>
2022-07-14 20:13:48 +0200

    .

---
+++ README.md
@@ -0,0 +1,9 @@
+# asht
+
+Super Simple API for Anystyle hosted on Vercel
+
+## Usage
+
+Send a post request to `(wherever)/api/style` with the body a list of citations and receive CSL back
+
+Optionally you can also specify a `?format=bibtex` or whatever
---
+++ api/style.rb
@@ -0,0 +1,36 @@
+# encoding: utf-8
+
+require 'anystyle'
+require 'json'
+
+Handler = Proc.new do |req, res|
+        ## Fucking ruby sets the encoding to ASCII for some reason
+        body = req.body().force_encoding('UTF-8')
+        if !body
+            res.status = 401
+            res['Content-type']= 'text/plain'
+            res.body= "Hey you didn't send anything dummy"
+            return
+        end
+
+        ## Get the 'format' parameter from the request query string
+        format = req.query['format']
+
+
+        parsed = AnyStyle.parse(body,{format: format || 'csl'})
+
+
+        res.status = 200
+        res['Access-Control-Allow-Origin'] = '*'
+        ## if then
+
+        if format == 'json' || !format || format == 'csl'
+          res['Content-Type'] = 'application/json; charset=utf-8'
+          res.body = JSON.generate(parsed)
+          return
+        end
+
+        res['Content-Type'] = 'text/plain; charset=utf-8'
+        res.body = parsed
+end
+
