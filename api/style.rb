54ffc797eced36622e47f77710fca7905e778433
Thomas F. K. Jorna <hello@tefkah.com>
2022-07-17 18:45:11 +0200

    add format option

--- api/style.rb
+++ api/style.rb
@@ -16,21 +16,24 @@ Handler = Proc.new do |req, res|
         ## Get the 'format' parameter from the request query string
         style = req.query()['format'] || 'csl'
 
-        pp style
-        pp :style
-        parsed = AnyStyle.parse(body,:style)
+        # pp style
+        # pp :style
+        # parsed = AnyStyle.parse(body,:style)
 
 
         res.status = 200
         res['Access-Control-Allow-Origin'] = '*'
         ## if then
 
-        if style == 'json' || !style|| style == 'csl'
+        if style == 'json' ||  style == 'csl' || !style 
+          pp "whoops"
           res['Content-Type'] = 'application/json; charset=utf-8'
+          parsed = AnyStyle.parse body, format: 'csl'
           res.body = JSON.generate(parsed)
         else
+          parsed = AnyStyle.parse body, format: style
           res['Content-Type'] = 'text/plain; charset=utf-8'
-          res.body = parsed
+          res.body = parsed.to_s
         end
 end
 
